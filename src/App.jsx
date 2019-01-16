d308136270807a376b17d76d933d8da1b5543c57
jorgealegria <jobaalegriaji@ittepic.edu.mx>
2019-01-15 21:14:06 -0700

    Create the index.js and App.jsx files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+    render(){
+        return(
+            <div>Music Master from app</div>
+        )
+    }
+}
+
+export default App;
